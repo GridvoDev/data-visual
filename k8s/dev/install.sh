bb37cce426d6ed0aaff08312592bff6b9b71377e
patrick176 <huangjc@refinece.com>
2017-03-15 18:11:46 +0800

    Update install.sh

--- k8s/dev/install.sh
+++ k8s/dev/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-kubectl get svc | grep -q data-visual
+kubectl -n gridvo get svc | grep -q data-visual
 if [ "$?" == "1" ];then
 	kubectl create -f data_visual-service.yaml --record
-	kubectl get svc | grep -q data-visual
+	kubectl -n gridvo get svc | grep -q data-visual
 	if [ "$?" == "0" ];then
 		echo "data_visual-service install success!"
 	else
@@ -11,10 +11,10 @@ if [ "$?" == "1" ];then
 else
 	echo "data_visual-service is exist!"
 fi
-kubectl get pods | grep -q data-visual
+kubectl -n gridvo get pods | grep -q data-visual
 if [ "$?" == "1" ];then
 	kubectl create -f data_visual-deployment.yaml --record
-	kubectl get pods | grep -q data-visual
+	kubectl -n gridvo get pods | grep -q data-visual
 	if [ "$?" == "0" ];then
 		echo "data_visual-deployment install success!"
 	else
@@ -22,13 +22,13 @@ if [ "$?" == "1" ];then
 	fi
 else
 	kubectl delete -f data_visual-deployment.yaml
-	kubectl get pods | grep -q data-visual
+	kubectl -n gridvo get pods | grep -q data-visual
 	while [ "$?" == "0" ]
 	do
-	kubectl get pods | grep -q data-visual
+	kubectl -n gridvo get pods | grep -q data-visual
 	done
 	kubectl create -f data_visual-deployment.yaml --record
-	kubectl get pods | grep -q data-visual
+	kubectl -n gridvo get pods | grep -q data-visual
 	if [ "$?" == "0" ];then
 		echo "data_visual-deployment update success!"
 	else
